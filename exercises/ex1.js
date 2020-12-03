4f57f78a8ac9746c740ffffc22d96ed8c1ea5cee
Andrew Carrigan <carrigan223@gmail.com>
2020-12-03 02:02:37 -0800

    starting ex 1'

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,1 +1,14 @@
 // My first Node.js program
+
+//process.stdout.write("Hello world");
+
+ console.log("Hello world");
+//console.log is essentially a standard output 
+//which is wrapped in dev convenience
+
+console.error("oops");
+
+// the > symbol is a redirect if used with the node command to dev/null
+//we can tell if its std.out or std.err, we have a different stream for
+// handling errors which have dev benefits, the third standard stream
+// is standard in much more touchy we will use it later with a package
